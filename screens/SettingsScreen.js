6f3de4cebee1a2fce2b1f301a2c6520b3ea7bc5e
zhuri <zhuriendrit@gmail.com>
2019-05-25 16:50:27 +0200

    added poster and icon

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { ExpoConfigView } from '@expo/samples';
 import {
-  StyleSheet,
   Text,
-  View,
-  SectionList
+  View
 } from 'react-native';
 
 export default class SettingsScreen extends React.Component {
   render() {
     return (
       <View>
-        <Text>Hello and welcome</Text>
+        <Text>Dear Friends, Dear Colleagues,</Text>
+        <Text>On behalf of the South-East European Ophthalmological Society, as the current president and as a representative of Kosovo Association of the Ophthalmologists, I am most pleased to invite you in the 16th SEEOS Congress which will be held in Prishtina, Republic of Kosovo, between “ May 31st until June 2nd 2019 “. This will be the Joint Congress with Kosovo Association of Ophthalmologists. Kosovo Association of Ophthalmologists was established in 2002 in Prishtina and is member of SEEOS, SOE and ICO. Currently, we have105 members, where among them are 80 ophthalmologists and 25 residents.</Text>
+        <Text>We are inviting the SEEOS members and ophthalmologists from all over the world to hold lectures, present posters or videos. Our intention is to provide an outstanding program, presented by world-renowned speakers. It will be a great opportunity to share experiences, talk about modern trends in the development of ophthalmologic care. We hope that you will also enjoy debates on controversial topics and discussions specifically aimed at the younger ophthalmologists, all of which will add interest and, at times, fun to the packed scientific program.</Text>
+        <Text>No SEEOS Congress would be complete without a great social program, which traditionally reflects the flavor of the host city, Prishtina is a unique modern city with the youngest population in Europe, has so much to offer, catering to every taste with its musical, artistic, and culinary traditions.</Text>
+        <Text>Let me again invite you to Prishtina next year on behalf of the organizing committee of the Joint Congress. Let's look forward to an interesting congress, a manifold collegial exchange, to meeting with old friends and making new friends.</Text>
       </View>
     )
   }
